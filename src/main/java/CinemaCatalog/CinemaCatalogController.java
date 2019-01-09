c0ea0a4a8b2d79a5074b423b36d4869172d846ca
Chen <i11007020@gmail.com>
2019-01-09 21:10:11 +0800

    commit

--- src/main/java/CinemaCatalog/CinemaCatalogController.java
+++ src/main/java/CinemaCatalog/CinemaCatalogController.java
@@ -76,7 +76,8 @@ public class CinemaCatalogController {
 			
 			
             System.out.println("Connect to database successfully");
-            return "Connect to database successfully:\n" + collection.countDocuments();
+            return "Connect to database successfully:\n" + collection.count();
+            
         } catch (Exception e) {  
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             return "Connect to database falily: "+e;
