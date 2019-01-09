034835fc07322f17561bded8fbafe14b0db38764
Chen <i11007020@gmail.com>
2019-01-09 21:01:12 +0800

    commit

--- src/main/java/CinemaCatalog/CinemaCatalogController.java
+++ src/main/java/CinemaCatalog/CinemaCatalogController.java
@@ -76,7 +76,7 @@ public class CinemaCatalogController {
 			
 			
             System.out.println("Connect to database successfully");
-            return "Connect to database successfully:\n" + collection.find(Filters.text("A Land Imaged"));
+            return "Connect to database successfully:\n" + collection.find(Filters.text("A Land Imaged")).first();
         } catch (Exception e) {  
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             return "Connect to database falily: "+e;
