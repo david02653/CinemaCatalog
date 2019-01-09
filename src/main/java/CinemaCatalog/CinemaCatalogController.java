1199cd1fe507a8e0859417d33a6a1a1b0ac28cbd
Chen <i11007020@gmail.com>
2019-01-09 19:51:00 +0800

    commit

--- src/main/java/CinemaCatalog/CinemaCatalogController.java
+++ src/main/java/CinemaCatalog/CinemaCatalogController.java
@@ -18,13 +18,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+
+import java.util.*;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.ServerAddress;
+
+
+
+
+
 @RestController
 public class CinemaCatalogController {
 	
 	@RequestMapping("/")
     public String index() 
     {
-        return "success";
+		try {  
+            ServerAddress serverAddress = new ServerAddress("localhost",27017);  
+            List<ServerAddress> addrs = new ArrayList<ServerAddress>();  
+            addrs.add(serverAddress);  
+              
+            //MongoCredential.createScramSha1Credential() 用戶, schema名稱,密碼 
+            MongoCredential credential = MongoCredential.createScramSha1Credential("cinema", "Movies", "cinema".toCharArray());  
+            List<MongoCredential> credentials = new ArrayList<MongoCredential>();  
+            credentials.add(credential);  
+              
+            // 通過認證連接MongoDB
+            MongoClient mongoClient = new MongoClient(addrs,credentials);
+              
+            // 連接到資料庫
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("Movie");  
+            System.out.println("Connect to database successfully");
+            return "Connect to database successfully";
+        } catch (Exception e) {  
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            return "Connect to database falily";
+        }
+		
+		
+        //return "success";
     }
 	
 	
