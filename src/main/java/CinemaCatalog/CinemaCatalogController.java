2acd29a0d59e7f37fa14a08473e3f9daafb4db25
Chen <i11007020@gmail.com>
2019-01-09 20:36:08 +0800

    commit

--- src/main/java/CinemaCatalog/CinemaCatalogController.java
+++ src/main/java/CinemaCatalog/CinemaCatalogController.java
@@ -1,5 +1,6 @@
 package CinemaCatalog;
 
+import org.bson.Document;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,6 +24,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.*;
 
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.ServerAddress;
@@ -38,20 +42,39 @@ public class CinemaCatalogController {
     public String index() 
     {
 		try {  
-            ServerAddress serverAddress = new ServerAddress("localhost",27017);  
-            List<ServerAddress> addrs = new ArrayList<ServerAddress>();  
-            addrs.add(serverAddress);  
-              
-            //MongoCredential.createScramSha1Credential() 用戶, schema名稱,密碼 
-            MongoCredential credential = MongoCredential.createScramSha1Credential("cinema", "Movies", "cinema".toCharArray());  
-            List<MongoCredential> credentials = new ArrayList<MongoCredential>();  
-            credentials.add(credential);  
-              
-            // 通過認證連接MongoDB
-            MongoClient mongoClient = new MongoClient(addrs,credentials);
-              
-            // 連接到資料庫
-            MongoDatabase mongoDatabase = mongoClient.getDatabase("Movie");  
+            
+			System.out.println("MongoDBConnect to database begin");
+            //連線到MongoDB服務 如果是遠端連線可以替換“localhost”為伺服器所在IP地址
+			
+            //MongoCredential.createScramSha1Credential()三個引數分別為 使用者名稱 資料庫名稱 密碼
+            MongoCredential credential = MongoCredential.createScramSha1Credential("cinema", "Movies", "cinema".toCharArray());
+            List<MongoCredential> credentials = new ArrayList<MongoCredential>();
+            credentials.add(credential);
+
+            MongoClientSettings settings = MongoClientSettings.builder()
+                    .credential(credential)
+                    .applyToSslSettings(builder -> builder.enabled(true))
+                    .applyToClusterSettings(builder -> 
+                        builder.hosts(Arrays.asList(new ServerAddress("mongodb://140.121.196.23", 4118))))
+                    .build();
+
+            
+            //通過連線認證獲取MongoDB連線
+            MongoClient mongoClient = MongoClients.create(settings);
+            
+            //連線到資料庫
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("Movie");
+            System.out.println("MongoDBConnect to database successfully");
+/*
+            //建立集合
+//            mongoDatabase.createCollection("test");
+//            System.out.println("集合建立成功");
+//選擇集合
+            MongoCollection<Document> collection = mongoDatabase.getCollection("test");
+            System.out.println("集合 test 選擇成功");
+			*/
+			
+			
             System.out.println("Connect to database successfully");
             return "Connect to database successfully";
         } catch (Exception e) {  
