f783fdc05260a2a8a0515ca2a33e4521a8fc6dee
Chen <i11007020@gmail.com>
2019-01-09 20:39:11 +0800

    commit

--- src/main/java/CinemaCatalog/CinemaCatalogController.java
+++ src/main/java/CinemaCatalog/CinemaCatalogController.java
@@ -55,7 +55,7 @@ public class CinemaCatalogController {
                     .credential(credential)
                     .applyToSslSettings(builder -> builder.enabled(true))
                     .applyToClusterSettings(builder -> 
-                        builder.hosts(Arrays.asList(new ServerAddress("mongodb://140.121.196.23", 4118))))
+                        builder.hosts(Arrays.asList(new ServerAddress("140.121.196.23", 4118))))
                     .build();
 
             
