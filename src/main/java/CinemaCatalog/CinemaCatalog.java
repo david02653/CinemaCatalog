181eb5cc74c076eb4ca01430f0816005867c96af
Chen <i11007020@gmail.com>
2019-01-24 23:18:27 +0800

    commmit

--- src/main/java/CinemaCatalog/CinemaCatalog.java
+++ src/main/java/CinemaCatalog/CinemaCatalog.java
@@ -105,7 +105,7 @@ public class CinemaCatalog {
 	{
 		String result = "";
 		try {
-			URL url = new URL("http://140.121.196.23:4105/newMovie?moviesID="+moviesID);
+			URL url = new URL("http://140.121.196.23:4105/newMovieOrdering?moviesID="+moviesID);
 			URLConnection urlConnection = url.openConnection();
 			
 			
