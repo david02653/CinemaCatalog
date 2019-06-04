7e2455f0c64b6e2ca57f1e0219e228b777d01799
Chen <i11007020@gmail.com>
2019-06-04 15:20:58 +0800

    add Feign

--- src/main/java/CinemaCatalog/FeignInterface.java
+++ src/main/java/CinemaCatalog/FeignInterface.java
@@ -1,14 +1,14 @@
 package CinemaCatalog;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name="api-gateway")
 public interface FeignInterface {
 	
-	@RequestMapping(value = "/ordering/getMovieFromOrderList?userID=1", method = RequestMethod.GET)
-    public String getMovieByID();
+	@RequestMapping(value = "/ordering/getMovieFromOrderList", method = RequestMethod.GET)
+    public String getMovieByID(@PathVariable("userID") String userID);
 	
 }
