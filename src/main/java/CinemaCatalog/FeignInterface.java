b73f65c2e5e1e143f77a7578b38f57c4e3b34f73
Chen <i11007020@gmail.com>
2019-06-04 16:13:10 +0800

    add Feign

--- src/main/java/CinemaCatalog/FeignInterface.java
+++ src/main/java/CinemaCatalog/FeignInterface.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name="api-gateway")
 public interface FeignInterface {
 	
-	@RequestMapping(value = "/ordering/getMovieFromOrderList", method = RequestMethod.GET)
+	@RequestMapping(value = "/ordering/getMovieFromOrderList?userID={userID}", method = RequestMethod.GET)
     public String getMovieByID(@RequestParam("userID") String userID);
 	
 }
