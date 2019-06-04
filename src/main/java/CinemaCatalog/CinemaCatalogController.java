4b6fb53ff5e2cb0920d8f7f700ae96073d67f7e6
Chen <i11007020@gmail.com>
2019-06-04 15:52:23 +0800

    add Feign

--- src/main/java/CinemaCatalog/CinemaCatalogController.java
+++ src/main/java/CinemaCatalog/CinemaCatalogController.java
@@ -18,9 +18,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Api(value = "CinemaCatalogController", tags = "與電影相關的所有一切都在這裡")
 @RestController
 public class CinemaCatalogController {
-	
-	CinemaCatalog cinema;
-	
 	@ApiOperation(value = "測試此伺服器是否成功連線", notes = "成功連線就回傳success")
 	@CrossOrigin(origins = "*")
 	@RequestMapping(value="/", method = RequestMethod.GET)
@@ -31,7 +28,7 @@ public class CinemaCatalogController {
 	
 	@ApiOperation(value = "拿到所有的電影資料", notes = "回傳資料")
 	@CrossOrigin(origins = "*")
-	@RequestMapping(value = "getAllMovies", method = RequestMethod.GET)
+	@RequestMapping(value = "/getAllMovies", method = RequestMethod.GET)
     public String getAllMovies()
     {
     	return CinemaCatalog.getAllMovies();
@@ -42,12 +39,12 @@ public class CinemaCatalogController {
 	@RequestMapping(value = "/getMovieByID", method = RequestMethod.GET)
     public String getMovieByID(@ApiParam(required = true, name = "userID", value = "使用者編號") @RequestParam("userID") String userID)
     {
-    	return cinema.getMovieByID(userID);
+    	return CinemaCatalog.getMovieByID(userID);
     }
 	
 	@ApiOperation(value = "拿到所有通知", notes = "回傳通知資料")
 	@CrossOrigin(origins = "*")
-	@RequestMapping(value = "getNotification", method = RequestMethod.GET)
+	@RequestMapping(value = "/getNotification", method = RequestMethod.GET)
     public String getNotification(@ApiParam(required = true, name = "userID", value = "使用者編號") @RequestParam("userID") String userID)
     {
     	return CinemaCatalog.getNotification(userID);
@@ -55,7 +52,7 @@ public class CinemaCatalogController {
 	
 	@ApiOperation(value = "購買電影", notes = "購買成功就回傳成功")
 	@CrossOrigin(origins = "*")
-	@RequestMapping(value = "orderingMovie", method = RequestMethod.GET)
+	@RequestMapping(value = "/orderingMovie", method = RequestMethod.GET)
     public String orderingMovie(@ApiParam(required = true, name = "ID", value = "電影編號")@RequestParam("moviesID") String moviesID)
     {
     	return CinemaCatalog.orderingMovie(moviesID);
