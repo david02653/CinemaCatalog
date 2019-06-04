c0b34fe7d0886c083771574eecb12b6f63160258
Chen <i11007020@gmail.com>
2019-06-04 16:23:31 +0800

    add Feign

--- src/main/java/CinemaCatalog/CinemaCatalogController.java
+++ src/main/java/CinemaCatalog/CinemaCatalogController.java
@@ -2,10 +2,22 @@ package CinemaCatalog;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
+import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +30,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Api(value = "CinemaCatalogController", tags = "與電影相關的所有一切都在這裡")
 @RestController
 public class CinemaCatalogController {
+	
+	@Autowired
+	FeignInterface feignInterface;
+	
 	@ApiOperation(value = "測試此伺服器是否成功連線", notes = "成功連線就回傳success")
 	@CrossOrigin(origins = "*")
 	@RequestMapping(value="/", method = RequestMethod.GET)
@@ -39,7 +55,68 @@ public class CinemaCatalogController {
 	@RequestMapping(value = "/getMovieByID", method = RequestMethod.GET)
     public String getMovieByID(@ApiParam(required = true, name = "userID", value = "使用者編號") @RequestParam("userID") String userID)
     {
-    	return CinemaCatalog.getMovieByID(userID);
+String result = "";
+		
+		
+		try {
+			
+			result = "[";
+			
+			/*
+			URL url = new URL("http://140.121.196.23:4139/ordering/getMovieFromOrderList?userID=1");
+			org.jsoup.nodes.Document xmlDoc =  Jsoup.parse(url, 3000);
+			String jaStr = xmlDoc.select("body").get(0).text();
+			*/
+			
+			System.out.println("-----------------------------------------------------------------------");
+			System.out.println(feignInterface.getMovieByID(userID));
+			System.out.println("-----------------------------------------------------------------------");
+			
+			String jaStr = feignInterface.getMovieByID(userID);
+			
+
+			
+			JSONArray jsonArray = JSONArray.fromObject(jaStr);
+			
+			
+			System.out.println("MongoDBConnect to database begin");
+			
+            MongoClient mongoClient = MongoClients.create("mongodb://cinema:cinema@140.121.196.23:4118");
+            
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("Movies");
+            System.out.println("MongoDBConnect to database successfully");
+            
+            MongoCollection<Document> collection = mongoDatabase.getCollection("Movie");
+            
+            
+            for(int i = 0; i < jsonArray.size(); i++) {
+            	JSONObject jsonObject = jsonArray.getJSONObject(i);
+            	
+            	BasicDBObject whereQuery = new BasicDBObject();
+            	
+            	whereQuery.put("_id", new ObjectId(jsonObject.getString("ObjectID")));
+            	
+                FindIterable<Document> fi = collection.find(whereQuery);
+                MongoCursor<Document> cursor = fi.iterator();
+                while(cursor.hasNext()) 
+                {
+                	result += cursor.next().toJson();
+                }
+            	
+                if(i < jsonArray.size() - 1)
+                	result += ",";
+            }
+			
+			
+			result += "]";
+			
+			return result;
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "{}";
+		}
     }
 	
 	@ApiOperation(value = "拿到所有通知", notes = "回傳通知資料")
