c3af364a9dd2fa5e4fa0d1c2d12379a3836c9a2b
Chen <i11007020@gmail.com>
2019-07-12 18:19:34 +0800

    add annotation @Autowired

--- src/test/java/CinemaCatalog/CinemaCatalogApplicationTests.java
+++ src/test/java/CinemaCatalog/CinemaCatalogApplicationTests.java
@@ -1,16 +1,48 @@
 package CinemaCatalog;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CinemaCatalogApplicationTests {
 
+	@Autowired
+	CinemaCatalogController cinemaCatalogController;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testNotification()
+	{
+		assertTrue( isJSONValid(cinemaCatalogController.getNotification("1")) );
+	}
+
+
+
+	public boolean isJSONValid(String test) {
+		try {
+			new JSONObject(test);
+		} catch (JSONException ex) {
+			// edited, to include @Arthur's comment
+			// e.g. in case JSONArray is valid as well...
+			try {
+				new JSONArray(test);
+			} catch (JSONException ex1) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
