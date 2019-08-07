d7ed25eacf7b4b38880d1734ad2bee17da1ad515
Chen <i11007020@gmail.com>
2019-08-07 15:20:29 +0800

    commit

--- src/main/java/cinemacatalog/CinemaCatalogApplication.java
+++ src/main/java/cinemacatalog/CinemaCatalogApplication.java
@@ -1,15 +1,16 @@
 package cinemacatalog;
 
+import com.soselab.vmamvserviceclient.annotation.EnableVmamvClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import com.soselab.vmamvserviceclient.annotation.EnableVmamvClient;
+
 
 @SpringBootApplication
+@EnableVmamvClient
 @EnableEurekaClient
 @EnableFeignClients
-@EnableVmamvClient
 public class CinemaCatalogApplication {
 
 	public static void main(String[] args) {
