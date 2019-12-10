db4e119d384805d28daa5962fe36c892011db8dd
i11007020 <i11007020@gmail.com>
2019-12-10 17:17:53 +0800

    commit

---
+++ src/main/java/cinemacatalog/CinemaCatalogController.java
@@ -0,0 +1,137 @@
+package cinemacatalog;
+
+import cinemacatalog.feign.NotificationInterface;
+import cinemacatalog.feign.OrderingInterface;
+import com.soselab.vmamvserviceclient.annotation.FeignRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@Api(value = "CinemaCatalogController", tags = "與電影相關的所有一切都在這裡")
+@RestController
+public class CinemaCatalogController {
+
+	@Autowired
+	OrderingInterface orderingInterface;
+	@Autowired
+	NotificationInterface notificationInterface;
+
+	private static final Logger logger = LoggerFactory.getLogger(CinemaCatalogController.class);
+	
+	@ApiOperation(value = "測試此伺服器是否成功連線", notes = "成功連線就回傳success")
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value="/", method = RequestMethod.GET)
+    public String index() 
+    {
+    	logger.info("success");
+		return "success";
+    }
+
+	@ApiOperation(value = "測試此伺服器是否成功連線", notes = "成功連線就回傳success")
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value="/index2", method = RequestMethod.GET)
+	public ResponseEntity<CinemaCatalog> index2()
+	{
+
+		return ResponseEntity.notFound().build();
+	}
+
+
+	// 模擬高duration
+	@ApiOperation(value = "測試是否成功連線", notes = "成功連線就回傳success")
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value="/index3", method = RequestMethod.GET)
+	public String index3()
+	{
+
+		String result = "wait success";
+
+
+		long num = (long)(Math.random() * 30);
+		try {
+			Thread.sleep(num);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+
+	@ApiOperation(value = "拿到所有的電影資料", notes = "回傳資料")
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value = "/getAllMovies", method = RequestMethod.GET)
+    public String getAllMovies()
+    {
+    	return CinemaCatalog.getAllMovies();
+    }
+
+
+	@FeignRequest(client = OrderingInterface.class, method = "getMovieByID", parameterTypes = String.class)
+	@ApiOperation(value = "利用ID找到某個電影", notes = "回傳某電影資料")
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value = "/getMovieByID", method = RequestMethod.GET)
+    public String getMovieByID(@ApiParam(required = true, name = "userID", value = "使用者編號") @RequestParam("userID") String userID)
+    {
+		String data = "";
+		try {
+			
+			data = orderingInterface.getMovieByID(userID);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		return CinemaCatalog.getMovieByID(userID,data);
+    }
+
+
+	@FeignRequest(client = OrderingInterface.class, method = "orderingMovie", parameterTypes = String.class)
+	@ApiOperation(value = "購買電影", notes = "購買成功就回傳成功")
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value = "/orderingMovie", method = RequestMethod.GET)
+    public String orderingMovie(@ApiParam(required = true, name = "ID", value = "電影編號")@RequestParam("moviesID") String moviesID)
+    {
+		String result = "";
+		try {
+			
+			result = orderingInterface.orderingMovie(moviesID);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return result;
+		
+    }
+
+
+	@FeignRequest(client = NotificationInterface.class, method = "getNotification", parameterTypes = String.class)
+	@ApiOperation(value = "拿到所有通知", notes = "回傳通知資料")
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value = "/getNotification", method = RequestMethod.GET)
+	public String getNotification(@ApiParam(required = true, name = "userID", value = "使用者編號") @RequestParam("userID") String userID)
+	{
+
+		String result = "";
+
+		try {
+			result = notificationInterface.getNotification(userID);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+}
+
+
+
---
+++ src/main/java/cinemacatalog/feign/OrderingInterface.java
@@ -0,0 +1,22 @@
+package cinemacatalog.feign;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// Assign service version to VMAMVS (unforce)
+//@TargetVersion("service version")
+@FeignClient("ordering")
+public interface OrderingInterface {
+
+//    @RequestMapping(value = "/", method = RequestMethod.GET)
+//    String index2();
+
+    @RequestMapping(value = "/getMovieFromOrderList", method = RequestMethod.GET)
+    String getMovieByID(@RequestParam("userID") String userID);
+
+    @RequestMapping(value = "/newMovieOrdering", method = RequestMethod.GET)
+    String orderingMovie(@RequestParam("moviesID") String moviesID);
+
+}
