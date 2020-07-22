ad7fd7b5235598ce990cca775be6361bf2125622
i11007020 <i11007020@gmail.com>
2020-07-22 19:54:32 +0800

    commit

--- src/main/java/cinemacatalog/feign/OrderingInterface.java
+++ src/main/java/cinemacatalog/feign/OrderingInterface.java
@@ -1,5 +1,6 @@
 package cinemacatalog.feign;
 
+import com.soselab.vmamvserviceclient.annotation.TargetVersion;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 // Assign service version to VMAMVS (unforce)
 //@TargetVersion("service version")
 @FeignClient("ordering")
+@TargetVersion("0.0.1-SNAPSHOT")
 public interface OrderingInterface {
 
 //    @RequestMapping(value = "/", method = RequestMethod.GET)
